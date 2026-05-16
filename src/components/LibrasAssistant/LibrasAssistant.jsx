fb1f4ba873c906bd3803646b5f23127cdf31aefb
PHYLIP RAPHAELL <117128320+PhylipRaphaell85@users.noreply.github.com>
2026-05-16 15:45:30 -0300

    s

--- src/components/LibrasAssistant/LibrasAssistant.jsx
+++ src/components/LibrasAssistant/LibrasAssistant.jsx
@@ -1,28 +1,20 @@
-import { useEffect } from "react";
-
 export default function LibrasAssistant() {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.type = "text/javascript";
-    script.src = "https://vlibras.gov.br/app/vlibras-plugin.js";
-    script.async = true;
-    document.head.appendChild(script);
+  return (
+    <div
+      dangerouslySetInnerHTML={{
+        __html: `
+          <div vw class="enabled">
+            <div vw-access-button class="active"></div>
+            <div vw-plugin-wrapper></div>
+          </div>
 
-    window.addEventListener("load", () => {
-      if (window.VLibras) {
-        new window.VLibras.Widget("https://vlibras.gov.br/app");
-      }
-    });
+          <script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
 
-    return () => {
-      if (window.VLibras) {
-        const vlibrasElement = document.querySelector("div[vw-access-button]");
-        if (vlibrasElement) {
-          vlibrasElement.remove();
-        }
-      }
-    };
-  }, []);
-
-  return null;
+          <script>
+            new window.VLibras.Widget('https://vlibras.gov.br/app');
+          </script>
+        `,
+      }}
+    />
+  );
 }
